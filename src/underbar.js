636b5a7a30ce9b7493fb40767ad3de501d40cfc7
Rustin Crandall <rustinpc@gmail.com>
2014-09-13 20:06:05 -0700

    Fixed "map" and "pluck" functions.

--- src/underbar.js
+++ src/underbar.js
@@ -138,6 +138,18 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        results.push(iterator(collection[i], i, collection));
+      }
+    }
+    else {
+      for (var key in collection) {
+        results.push(iterator(collection[key], key, collection));
+      }
+    }
+    return results;
   };
 
   /*
